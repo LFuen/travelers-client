75bbf97483281553538d80f515875a768a484055
Lili Fuentes <lile7886@gmail.com>
2024-06-19 14:23:54 -0400

    updated api endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const api = {
-  API_ENDPOINT: "travelers.cqlz5glh6vqe.us-east-1.rds.amazonaws.com/api",
+  API_ENDPOINT: "http://travelers.cqlz5glh6vqe.us-east-1.rds.amazonaws.com/api",
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: process.env.TOKEN_KEY,
 };
